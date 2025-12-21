4455c094c6b52d12fcea588b2c2e7e6a2d50cba5
shivanshunigam <shivanshunigam6@gmail.com>
2025-12-21 16:40:07 +0530

    daily planner

--- script.js
+++ script.js
@@ -18,69 +18,101 @@ function openFeature() {
 }
 openFeature();
 
-let form = document.querySelector('.addTask form');
-let taskInput = document.querySelector('.addTask form #task-input')
-let taskDetailsInput = document.querySelector('.addTask form textarea');
-let taskCheckbox = document.querySelector('.addTask form #check');
- 
+
+
 // localStorage.clear();
-var currentTask = []
-if(localStorage.getItem('currentTask')){
-  currentTask=JSON.parse(localStorage.getItem('currentTask'))
-    
-}else{
-    console.log('task list is empty');
-    
-  
-  
-    
-}
+
+
+function todolist() {
+    var currentTask = []
+    if (localStorage.getItem('currentTask')) {
+        currentTask = JSON.parse(localStorage.getItem('currentTask'))
+
+    } else {
+        console.log('task list is empty');
 
 
 
 
-function renderTask() {
-      localStorage.setItem('currentTask',JSON.stringify(currentTask));
+    }
 
-    var allTask = document.querySelector('.allTask')
-    var sum = '';
-    currentTask.forEach((elem,idx) => {
-        sum += `<div class="task">
+
+
+
+    function renderTask() {
+        localStorage.setItem('currentTask', JSON.stringify(currentTask));
+
+        var allTask = document.querySelector('.allTask')
+        let sum = '';
+        currentTask.forEach((elem, idx) => {
+            sum += `<div class="task">
         <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
         <button id=${idx}>mark as Completed</button>
         
         </div>`
 
+        })
+
+        allTask.innerHTML = sum;
+        document.querySelectorAll('.task button').forEach((btn) => {
+            btn.addEventListener('click', () => {
+                currentTask.splice(btn.id, 1)
+                renderTask()
+            })
+        })
+
+    }
+    renderTask();
+
+    let form = document.querySelector('.addTask form');
+    let taskInput = document.querySelector('.addTask form #task-input')
+    let taskDetailsInput = document.querySelector('.addTask form textarea');
+    let taskCheckbox = document.querySelector('.addTask form #check');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        currentTask.push(
+            {
+                task: taskInput.value,
+                details: taskDetailsInput.value,
+                imp: taskCheckbox.checked
+            }
+        )
+        renderTask()
+        taskCheckbox.checked = false
+        taskInput.value = ""
+        taskDetailsInput.value = ""
+
+    })
+}
+todolist();
+
+function dailyPlanner() {
+    var dayPlanner = document.querySelector('.day-planner')
+
+    var dayPlanData = JSON.parse(localStorage.getItem('dayPlanData')) || {}
+
+    var hours = Array.from({ length: 18 }, (_, idx) => `${6 + idx}:00 - ${7 + idx}:00`)
+
+    let wholeDaysum = ''
+
+    hours.forEach((elem, idx) => {
+        var savedData = dayPlanData[idx] || ''
+        wholeDaysum += `
+        <div class="day-planner-time">
+            <p>${elem}</p>
+            <input id="${idx}" type="text" placeholder="..." value="${savedData}">
+        </div>`
     })
 
-    allTask.innerHTML = sum;
-   
+    dayPlanner.innerHTML = wholeDaysum
+
+    document.querySelectorAll('.day-planner input').forEach((elem) => {
+        elem.addEventListener('input', () => {
+            dayPlanData[elem.id] = elem.value
+            localStorage.setItem('dayPlanData', JSON.stringify(dayPlanData))
+        })
+    })
 }
-renderTask();
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    currentTask.push(
-        {
-            task: taskInput.value,
-            details: taskDetailsInput.value,
-            imp: taskCheckbox.checked
-        }
-    )
-     renderTask()
-   
-      
-    taskInput.value=''
-     taskDetailsInput.value=''
-     taskCheckbox.checked=false
-     })
-
-   
-
-var markscomplebtn= document.querySelectorAll('.task button')
-markscomplebtn.forEach((btn)=>{
-btn.addEventListener('click',()=>{
- currentTask.splice
-
-})
-})
+dailyPlanner()
