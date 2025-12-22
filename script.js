7b68eda6e0717708e932b01021dfeca1a5dac7c4
shivanshunigam <shivanshunigam6@gmail.com>
2025-12-22 15:25:26 +0530

    motivational quotes

--- script.js
+++ script.js
@@ -116,3 +116,18 @@ function dailyPlanner() {
 }
 
 dailyPlanner()
+
+
+function motivationalQuotes(){
+    var motivationQuote=document.querySelector('.motivation-2 h1')
+var motivationAuthor=document.querySelector('.motivation-3 h2')
+
+async function fetchQuote(){
+    let response= await fetch('https://api.quotable.io/random')
+    let data=await response.json()
+motivationQuote.innerHTML=data.content    
+motivationAuthor.innerHTML=data.author    
+}
+fetchQuote()
+}
+motivationalQuotes();
